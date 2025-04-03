86d488a339ed59c5532ad96548d6526f13c51ea4
u200165282 <noreply@example.com>
2025-04-03 09:19:54 +0800

    update: orangepiaicode添加settings.html打包

--- extensions/xunlongaicode/webview-ui/src/apps/settings/components/Common.tsx
+++ extensions/xunlongaicode/webview-ui/src/apps/settings/components/Common.tsx
@@ -1,8 +1,21 @@
 import { FC } from 'react';
+import { Button } from '../../../components/ui';
 
 const Common: FC = () => {
 	return <div>
-		<h1>Common</h1>
+		<div className='text-[24px] font-medium mb-[16px]'>通用</div>
+
+		<div className='font-medium text-[16px] mb-[12px]'>账户</div>
+
+		<div className='text-[14px] mb-[12px] text-foreground/70'>
+			当前登录账号：ZZZ
+		</div>
+
+		<div className='mb-[24px]'>
+			<Button className='w-[140px] rounded'>退出登录</Button>
+		</div>
+
+		<div className='font-medium text-[16px] mb-[12px]'>主题</div>
 	</div>
 }
 
--- extensions/xunlongaicode/webview-ui/vite.config.ts
+++ extensions/xunlongaicode/webview-ui/vite.config.ts
@@ -16,6 +16,10 @@ export default defineConfig({
 		outDir: "build",
 		reportCompressedSize: false,
 		rollupOptions: {
+			input: {
+				main: path.resolve(__dirname, "index.html"),
+				settings: path.resolve(__dirname, "settings.html"),
+			},
 			output: {
 				entryFileNames: `assets/[name].js`,
 				chunkFileNames: `assets/[name].js`,

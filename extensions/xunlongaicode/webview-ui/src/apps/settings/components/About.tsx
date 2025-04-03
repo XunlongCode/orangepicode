c54cb0a0c0ac7d4d0f993ab5c4ef95898dbb4e6a
u200165282 <noreply@example.com>
2025-04-03 11:43:51 +0800

    update: 关于页面

--- extensions/xunlongaicode/webview-ui/src/apps/settings/components/About.tsx
+++ extensions/xunlongaicode/webview-ui/src/apps/settings/components/About.tsx
@@ -5,8 +5,20 @@ const About: FC = () => {
 	const { t } = useAppTranslation()
 
 	return <div>
-		<div className='text-[24px] font-medium mb-[16px]'>
-			{t("about", { ns: "settingsApp" })}
+		<div className='text-[24px] font-medium mb-[24px]'>
+			{t("aboutOrangePiAICode", { ns: "settingsApp" })}
+		</div>
+
+		<div className='flex flex-col gap-2.5'>
+			<a href="https://github.com/XunlongCode/orangepicode" target='_blank' className='text-primary text-[14px] font-medium'>
+				{t("userAgreement", { ns: "settingsApp" })}
+			</a>
+			<a href="https://github.com/XunlongCode/orangepicode" target='_blank' className='text-primary text-[14px] font-medium'>
+				{t("privacyPolicy", { ns: "settingsApp" })}
+			</a>
+			<a href="https://github.com/XunlongCode/orangepicode/blob/main/LICENSE.txt" target='_blank' className='text-primary text-[14px] font-medium'>
+				{t("opensourceSoftwareStatement", { ns: "settingsApp" })}
+			</a>
 		</div>
 	</div>
 }

20e75ae39afa0f4cc5e4c9d548237f5c4174c577
u200165282 <noreply@example.com>
2025-04-07 14:27:48 +0800

    update: 去除copilot指令

--- src/vs/workbench/contrib/chat/browser/chat.contribution.ts
+++ src/vs/workbench/contrib/chat/browser/chat.contribution.ts
@@ -50,10 +50,16 @@ import { LanguageModelToolsExtensionPointHandler } from '../common/tools/languag
 import { BuiltinToolsContribution } from '../common/tools/tools.js';
 import { IVoiceChatService, VoiceChatService } from '../common/voiceChatService.js';
 import { EditsChatAccessibilityHelp, PanelChatAccessibilityHelp, QuickChatAccessibilityHelp } from './actions/chatAccessibilityHelp.js';
-import { CopilotTitleBarMenuRendering, registerChatActions } from './actions/chatActions.js';
-import { ACTION_ID_NEW_CHAT, registerNewChatActions } from './actions/chatClearActions.js';
+import {
+	CopilotTitleBarMenuRendering,
+	// registerChatActions
+} from './actions/chatActions.js';
+import {
+	ACTION_ID_NEW_CHAT,
+	//  registerNewChatActions
+} from './actions/chatClearActions.js';
 import { CodeBlockActionRendering, registerChatCodeBlockActions, registerChatCodeCompareBlockActions } from './actions/chatCodeblockActions.js';
-import { registerChatContextActions } from './actions/chatContextActions.js';
+// import { registerChatContextActions } from './actions/chatContextActions.js';
 import { registerChatCopyActions } from './actions/chatCopyActions.js';
 import { registerChatDeveloperActions } from './actions/chatDeveloperActions.js';
 import { ChatSubmitAction, registerChatExecuteActions } from './actions/chatExecuteActions.js';
@@ -61,7 +67,7 @@ import { registerChatFileTreeActions } from './actions/chatFileTreeActions.js';
 import { ChatGettingStartedContribution } from './actions/chatGettingStarted.js';
 import { registerChatExportActions } from './actions/chatImportExport.js';
 import { registerMoveActions } from './actions/chatMoveActions.js';
-import { registerQuickChatActions } from './actions/chatQuickInputActions.js';
+// import { registerQuickChatActions } from './actions/chatQuickInputActions.js';
 import { registerChatTitleActions } from './actions/chatTitleActions.js';
 import { IChatAccessibilityService, IChatCodeBlockContextProviderService, IChatWidgetService, IQuickChatService } from './chat.js';
 import { ChatAccessibilityService } from './chatAccessibilityService.js';
@@ -69,7 +75,7 @@ import './chatAttachmentModel.js';
 import { ChatMarkdownAnchorService, IChatMarkdownAnchorService } from './chatContentParts/chatMarkdownAnchorService.js';
 import { ChatInputBoxContentProvider } from './chatEdinputInputContentProvider.js';
 import { ChatEditingEditorAccessibility } from './chatEditing/chatEditingEditorAccessibility.js';
-import { registerChatEditorActions } from './chatEditing/chatEditingEditorActions.js';
+// import { registerChatEditorActions } from './chatEditing/chatEditingEditorActions.js';
 import { ChatEditingEditorOverlay } from './chatEditing/chatEditingEditorOverlay.js';
 import { ChatEditingService } from './chatEditing/chatEditingServiceImpl.js';
 import { ChatEditor, IChatEditorOptions } from './chatEditor.js';
@@ -476,20 +482,20 @@ registerWorkbenchContribution2(ChatEditingEditorAccessibility.ID, ChatEditingEdi
 registerWorkbenchContribution2(ChatEditingEditorOverlay.ID, ChatEditingEditorOverlay, WorkbenchPhase.AfterRestored);
 registerWorkbenchContribution2(ChatEditingEditorContextKeys.ID, ChatEditingEditorContextKeys, WorkbenchPhase.AfterRestored);
 
-registerChatActions();
+// registerChatActions();
 registerChatCopyActions();
 registerChatCodeBlockActions();
 registerChatCodeCompareBlockActions();
 registerChatFileTreeActions();
 registerChatTitleActions();
 registerChatExecuteActions();
-registerQuickChatActions();
+// registerQuickChatActions();
 registerChatExportActions();
 registerMoveActions();
-registerNewChatActions();
-registerChatContextActions();
+// registerNewChatActions();
+// registerChatContextActions();
 registerChatDeveloperActions();
-registerChatEditorActions();
+// registerChatEditorActions();
 registerChatToolActions();
 
 registerEditorFeature(ChatPasteProvidersFeature);

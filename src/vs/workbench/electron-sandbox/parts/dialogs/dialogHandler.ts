87b4c1721542bb5c1881cbb51683f2bc51fc9707
u200165282 <noreply@example.com>
2025-04-10 13:34:08 +0800

    update: 关于弹窗

--- src/vs/workbench/electron-sandbox/parts/dialogs/dialogHandler.ts
+++ src/vs/workbench/electron-sandbox/parts/dialogs/dialogHandler.ts
@@ -83,7 +83,7 @@ export class NativeDialogHandler extends AbstractDialogHandler {
 
 		const detailString = (useAgo: boolean): string => {
 			return localize({ key: 'aboutDetail', comment: ['Electron, Chromium, Node.js and V8 are product names that need no translation'] },
-				"Version: {0}\nVSCode Version: {9}\nCommit: {1}\nDate: {2}\nElectron: {3}\nElectronBuildId: {4}\nChromium: {5}\nNode.js: {6}\nV8: {7}\nOS: {8}",
+				`Version: {0}\nVSCode Version: ${this.productService.vscodeVersion}\nCommit: {1}\nDate: {2}\nElectron: {3}\nElectronBuildId: {4}\nChromium: {5}\nNode.js: {6}\nV8: {7}\nOS: {8}`,
 				version,
 				this.productService.commit || 'Unknown',
 				this.productService.date ? `${this.productService.date}${useAgo ? ' (' + fromNow(new Date(this.productService.date), true) + ')' : ''}` : 'Unknown',
@@ -93,7 +93,6 @@ export class NativeDialogHandler extends AbstractDialogHandler {
 				process.versions['node'],
 				process.versions['v8'],
 				`${osProps.type} ${osProps.arch} ${osProps.release}${isLinuxSnap ? ' snap' : ''}`,
-				this.productService.vscodeVersion
 			).replace('\n', `\n${releaseString} ${this.productService.release || 'Unknown'}\n`);
 		};
 
